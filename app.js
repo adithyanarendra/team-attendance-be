30a1dbe23ed13028e64ebac8201db72fc43cf747
M N Adithya <adithyamnarendra@gmail.com>
2025-04-25 22:31:36 +0530

    feat: added a /health route to ping for bot health

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const bodyParser = require('body-parser');
 require('./config/dotenv');
 
 const slackRoutes = require('./routes/slackRoutes');
+const healthRoute = require('./routes/health');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/slack', slackRoutes);
+app.use('/health', healthRoute);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
